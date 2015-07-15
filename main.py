b6d50872f9b77f376d6d5e6a47b2fe3821f77a93
Gabor Tanz <izolight@gmail.com>
2015-07-15 14:18:37 +0200

    Regex for splitting header.

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ s1 = nntplib.NNTP(host=auth[1]['server'],user=auth[1]['username'],password=auth[
 #s2 = nntplib.NNTP(host=auth[2]['server'],user=auth[2]['username'],password=auth[2]['password'])
 
 s0.group('korea.binaries.tv')
-resp, overviews = s0.over((44100000,44110000))
+resp, overviews = s0.over((44100000,44100010))
 subjects = {}
-for ov in overviews:
-#	cur_sub = ov[1]['subject']
-	cur_sub = re.split("\(\d{1,4}\/\d{1,4}\)",ov[1]['subject'])[0]
-	if cur_sub in subjects:
-		subjects[cur_sub] += 1
-	else:
-		subjects[cur_sub] = 1
-for s_key, s in subjects.items():
-	print('Subject %s %d' % (s_key.encode(encoding,errors).decode(encoding),s))
+for number, header in overviews:
+	subject, part, total_parts, filename, segement, total_segements =  re.match(r"(.*) \[(\d{1,4})\/(\d{1,4})\] \- \"(.*)\" yEnc \((\d{1,4})\/(\d{1,4})", header['subject']).groups()
+	print(subject)
+#	print(header['subject'])
+#	cur_sub = re.split("\(\d{1,4}\/\d{1,4}\)",ov[1]['subject'])[0]
+#	if cur_sub in subjects:
+#		subjects[cur_sub] += 1
+#	else:
+#		subjects[cur_sub] = 1
+#for s_key, s in subjects.items():
+#	print('Subject %s %d' % (s_key.encode(encoding,errors).decode(encoding),s))
