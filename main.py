807a265a9476bd85d2d3dc2800832bd94a219720
Gabor Tanz <izolight@gmail.com>
2015-07-15 18:03:12 +0200

    building basic structure for indexing.

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ s0 = nntplib.NNTP(host=auth[0]['server'],user=auth[0]['username'],password=auth[
 s1 = nntplib.NNTP(host=auth[1]['server'],user=auth[1]['username'],password=auth[1]['password'])
 #s2 = nntplib.NNTP(host=auth[2]['server'],user=auth[2]['username'],password=auth[2]['password'])
 
+pattern = re.compile(r"(?P<subject>.+?) \[(?P<part>\d{1,4})\/(?P<total_parts>\d{1,4})\] \- \"(?P<filename>.+?)\" yEnc \((?P<segment>\d{1,4})\/(?P<total_segments>\d{1,4})")
+
 s0.group('korea.binaries.tv')
 resp, overviews = s0.over((44100000,44100010))
 subjects = {}
+
 for number, header in overviews:
-	subject, part, total_parts, filename, segement, total_segements =  re.match(r"(.*) \[(\d{1,4})\/(\d{1,4})\] \- \"(.*)\" yEnc \((\d{1,4})\/(\d{1,4})", header['subject']).groups()
-	print(subject)
+	subject, part_nr, total_parts, filename, segment_nr, total_segments = pattern.match(header['subject']).groups()
+	segment = {'message-id':header['message-id'], 'bytes':header[':bytes']}
+	part = {'name':filename, 'total_segments':total_segments, 'poster':header['from'], 'date':header['date'], segment_nr:segment}
+	subjects[subject] = {'total_parts':total_parts, part_nr:part}
 #	print(header['subject'])
 #	cur_sub = re.split("\(\d{1,4}\/\d{1,4}\)",ov[1]['subject'])[0]
 #	if cur_sub in subjects:
@@ -22,3 +27,14 @@ for number, header in overviews:
 #		subjects[cur_sub] = 1
 #for s_key, s in subjects.items():
 #	print('Subject %s %d' % (s_key.encode(encoding,errors).decode(encoding),s))
+for s in subjects:
+	print(subjects[s])
+
+#construct for saving stuff
+#{'subject':
+#	{'total_parts':number, part_nr: # subject
+#		{'name':filename, 'total_segments':number, 'poster':from, 'date':date, segment_nr: #part
+#			{'bytes':bytes, 'message_id':message_id} # segment
+#		}
+#	}
+#}
