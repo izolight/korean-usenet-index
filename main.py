82609c7bc3a2c74794d2876fa4fa99529ff3fa95
Gabor Tanz <izolight@gmail.com>
2015-07-19 20:12:58 +0200

    testing some output.

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+#! /usr/bin/python3
 import nntplib, re
 from config import auth
+from time import time
 
 encoding = 'utf-8'
 errors = 'ignore'
 
 s0 = nntplib.NNTP(host=auth[0]['server'],user=auth[0]['username'],password=auth[0]['password'])
-#s1 = nntplib.NNTP(host=auth[1]['server'],user=auth[1]['username'],password=auth[1]['password'])
-#s2 = nntplib.NNTP(host=auth[2]['server'],user=auth[2]['username'],password=auth[2]['password'])
 
 pattern = re.compile(r"(?P<subject>.+?) \[(?P<part>\d{1,4})\/(?P<total_parts>\d{1,4})\] \- \"(?P<filename>.+?)\" yEnc \((?P<segment>\d{1,4})\/(?P<total_segments>\d{1,4})")
 
+n_headers = 50000
 resp, count ,first, last, name = s0.group('korea.binaries.tv')
-resp, overviews = s0.over((last-100,last))
+t0 = time()
+resp, overviews = s0.over((last-n_headers,last))
+t1 = time()
+print('got %s headers in %f seconds' % (n_headers,(t1-t0)))
 subjects = {}
 
 for number, header in overviews:
@@ -26,22 +30,8 @@ for number, header in overviews:
 	else:
 		part = {'name':filename, 'total_segments':total_segments, 'poster':header['from'], 'date':header['date'], 'segments':{segment_nr:segment}}
 		subjects[subject] = {'total_parts':total_parts, 'parts':{part_nr:part}}
-#	print(header['subject'])
-#	cur_sub = re.split("\(\d{1,4}\/\d{1,4}\)",ov[1]['subject'])[0]
-#	if cur_sub in subjects:
-#		subjects[cur_sub] += 1
-#	else:
-#		subjects[cur_sub] = 1
-#for s_key, s in subjects.items():
-#	print('Subject %s %d' % (s_key.encode(encoding,errors).decode(encoding),s))
-#for s in subjects:
-#	print(subjects[s])
-#len(subjects)
-#construct for saving stuff
-#{'subject':
-#	{'total_parts':number, part_nr: # subject
-#		{'name':filename, 'total_segments':number, 'poster':from, 'date':date, segment_nr: #part
-#			{'bytes':bytes, 'message_id':message_id} # segment
-#		}
-#	}
-#}
+t2 = time()
+print('organized %s headers in %f seconds' % (n_headers,(t2-t1)))
+
+for s in subjects:
+	print('%s: %s of %s parts' % (s,len(subjects[s]['parts']),subjects[s]['total_parts']))
