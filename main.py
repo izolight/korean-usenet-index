526f2aef6afc9b9d9282efd141f6e96e5ebfe944
Gabor Tanz <izolight@gmail.com>
2015-07-19 19:38:59 +0200

    implement proper data structure.

--- main.py
+++ main.py
@@ -5,20 +5,27 @@ encoding = 'utf-8'
 errors = 'ignore'
 
 s0 = nntplib.NNTP(host=auth[0]['server'],user=auth[0]['username'],password=auth[0]['password'])
-s1 = nntplib.NNTP(host=auth[1]['server'],user=auth[1]['username'],password=auth[1]['password'])
+#s1 = nntplib.NNTP(host=auth[1]['server'],user=auth[1]['username'],password=auth[1]['password'])
 #s2 = nntplib.NNTP(host=auth[2]['server'],user=auth[2]['username'],password=auth[2]['password'])
 
 pattern = re.compile(r"(?P<subject>.+?) \[(?P<part>\d{1,4})\/(?P<total_parts>\d{1,4})\] \- \"(?P<filename>.+?)\" yEnc \((?P<segment>\d{1,4})\/(?P<total_segments>\d{1,4})")
 
-s0.group('korea.binaries.tv')
-resp, overviews = s0.over((44100000,44100010))
+resp, count ,first, last, name = s0.group('korea.binaries.tv')
+resp, overviews = s0.over((last-100,last))
 subjects = {}
 
 for number, header in overviews:
 	subject, part_nr, total_parts, filename, segment_nr, total_segments = pattern.match(header['subject']).groups()
 	segment = {'message-id':header['message-id'], 'bytes':header[':bytes']}
-	part = {'name':filename, 'total_segments':total_segments, 'poster':header['from'], 'date':header['date'], segment_nr:segment}
-	subjects[subject] = {'total_parts':total_parts, part_nr:part}
+	if subject in subjects:
+		if part_nr in subjects[subject]['parts']:
+			subjects[subject]['parts'][part_nr]['segments'][segment_nr] = segment
+		else:		
+			part = {'name':filename, 'total_segments':total_segments, 'poster':header['from'], 'date':header['date'], 'segments':{segment_nr:segment}}
+			subjects[subject]['parts'][part_nr] = part
+	else:
+		part = {'name':filename, 'total_segments':total_segments, 'poster':header['from'], 'date':header['date'], 'segments':{segment_nr:segment}}
+		subjects[subject] = {'total_parts':total_parts, 'parts':{part_nr:part}}
 #	print(header['subject'])
 #	cur_sub = re.split("\(\d{1,4}\/\d{1,4}\)",ov[1]['subject'])[0]
 #	if cur_sub in subjects:
@@ -27,9 +34,9 @@ for number, header in overviews:
 #		subjects[cur_sub] = 1
 #for s_key, s in subjects.items():
 #	print('Subject %s %d' % (s_key.encode(encoding,errors).decode(encoding),s))
-for s in subjects:
-	print(subjects[s])
-
+#for s in subjects:
+#	print(subjects[s])
+#len(subjects)
 #construct for saving stuff
 #{'subject':
 #	{'total_parts':number, part_nr: # subject
